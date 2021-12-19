ed06819640e98070c1d7f3c2a9b8859c9d53f660
JorgeLuisRupay <jorgeluis17rh17@gmail.com>
2021-12-19 18:25:02 -0500

    v1 build

---
+++ precache-manifest.69aefb28cc242b58d4ea575d07c206bc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cdaa0939bd42a73fa2",
+    "url": "/css/app.b5c83e2f.css"
+  },
+  {
+    "revision": "9f8d6ef568e1058fe429",
+    "url": "/css/artistas.0f8add62.css"
+  },
+  {
+    "revision": "dfbb0de58fd395ed5e43",
+    "url": "/css/canciones.6d861522.css"
+  },
+  {
+    "revision": "4c003213a234faa2cdb5",
+    "url": "/css/estaciones.edd30a30.css"
+  },
+  {
+    "revision": "31e36a1e46f4f46549048766aadeec0e",
+    "url": "/img/foxbel-music.31e36a1e.png"
+  },
+  {
+    "revision": "961b76ae2304307524a55ff59f76449b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d182f62a14925b04c17b",
+    "url": "/js/albums.0c953f12.js"
+  },
+  {
+    "revision": "94cdaa0939bd42a73fa2",
+    "url": "/js/app.58aa5ef7.js"
+  },
+  {
+    "revision": "9f8d6ef568e1058fe429",
+    "url": "/js/artistas.688d1856.js"
+  },
+  {
+    "revision": "f9ad31fd26b0c0a0e94c",
+    "url": "/js/baladas.b0ff2ad1.js"
+  },
+  {
+    "revision": "dfbb0de58fd395ed5e43",
+    "url": "/js/canciones.2590d405.js"
+  },
+  {
+    "revision": "45901c2a3f6012221e29",
+    "url": "/js/chunk-vendors.624544ff.js"
+  },
+  {
+    "revision": "4c003213a234faa2cdb5",
+    "url": "/js/estaciones.5250fc50.js"
+  },
+  {
+    "revision": "5e8b0fbbe34724e4ccbd",
+    "url": "/js/metal.30b85fa1.js"
+  },
+  {
+    "revision": "9d9ac0cf4747c0f55a1d",
+    "url": "/js/parabailar.33d7b7ad.js"
+  },
+  {
+    "revision": "16464cdaf2138fedb255",
+    "url": "/js/rock90.0a742a4b.js"
+  },
+  {
+    "revision": "dcc479b36485cee152003f4a9c1a7c3a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
